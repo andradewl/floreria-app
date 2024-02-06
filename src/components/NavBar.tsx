c3c1010472a26e290cc70b55fcf8cc51dba0b73f
YaelWebLab <yaelweblab@gmail.com>
2024-02-06 17:53:11 -0600

    1.2.2 Custom NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,71 +1,150 @@
-import {
-  AppBar,
-  
-  Tab,
-  Tabs,
-  Toolbar,
-  useMediaQuery,
-  useTheme,
-} from "@mui/material";
-import DrawerComp from "./Drawer";
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import Logo from '../assets/LOGO.webp'
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import Person2Icon from '@mui/icons-material/Person2';
-import { Link } from 'react-router-dom';
-import { useState } from "react";
 
+const pages = ['Inicio', 'Productos', 'Nosotros', 'Contacto'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
-function NavBar(){
-  
-    const [value, setValue] = useState();
-    const theme = useTheme();
-    console.log(theme);
-    const isMatch = useMediaQuery(theme.breakpoints.down("md"));
-    console.log(isMatch);
+function ResponsiveAppBar() {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
 
-    return(
-        <AppBar sx={{ background: "#ffff", padding:'20px', boxShadow:'none', position:'sticky'}}>
-                <Toolbar>
-                    <img src="https://floreriadaisysgarden.com.mx/wp-content/uploads/2021/03/Recurso-3@4x.png" alt="React Image" width='115'/>
-                    {isMatch ? (
-                    <>
-                        <DrawerComp />
-                    </>
-                    ) : (
-                    <>
-                        <Tabs
-                            sx={{ marginLeft: "auto", color:'purple'}}
-                            indicatorColor="secondary"
-                           
-                            value={value}
-                            onChange={(e, value) => setValue(value)}
-                        >
-                            <Link to="/">
-                                <Tab label="Inicio"/>
-                            </Link>
-                            <Link to="/Productos">
-                                <Tab label="Productos" />
-                            </Link>
-                            <Link to="/Nosotros">
-                                <Tab label="Nosotros" />
-                            </Link>
-                            <Link to="/Contacto">
-                                <Tab label="Contacto" />
-                            </Link>
-                        </Tabs>
-                        <div style={{marginLeft:'auto'}}>
-                            <SearchIcon color="disabled" sx={{padding:'3px'}}/>
-                            <ShoppingCartIcon color="disabled" sx={{padding:'3px'}}/>
-                            <Person2Icon color="disabled" sx={{padding:'3px'}}/>
-                        </div>
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
 
-                    </>
-                    )}
-                </Toolbar>
-            
-        </AppBar>
-         
-    )
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <AppBar position="static"sx={{ backgroundColor: '#C8A2CB' }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          
+          
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <img
+        src={Logo}
+        alt="Logo"
+        style={{ width: '100px', height: '70px', marginRight: '1rem' }} // Ajusta el estilo según tus necesidades
+      />
+      <Typography
+        variant="h5"
+        noWrap
+        component="a"
+        href="#app-bar-with-responsive-menu"
+        sx={{
+          mr: 2,
+          display: { xs: 'flex', md: 'none' },
+          flexGrow: 1,
+          fontFamily: 'monospace',
+          fontWeight: 700,
+          letterSpacing: '.3rem',
+          color: 'inherit',
+          textDecoration: 'none',
+        }}
+      >
+        
+      </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+                <SearchIcon color="disabled" sx={{padding:'3px'}}/>
+                <ShoppingCartIcon color="disabled" sx={{padding:'3px'}}/>
+                <Person2Icon color="disabled" sx={{padding:'3px'}}/>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 }
-
-export default NavBar
+export default ResponsiveAppBar;
