c425c6d343f24b71a99ac58d639a50084fb10555
YaelWebLab <yaelweblab@gmail.com>
2024-04-21 02:02:05 -0600

    3.9.5 Adicion Funcionalidades de Administrador Estatus Envio

--- src/config/backEndAdmin/backEstatusAdmin.tsx
+++ src/config/backEndAdmin/backEstatusAdmin.tsx
@@ -1,8 +1,5 @@
 import { db } from "../firfebase";
-import {
-  collection,
-  getDocs,
-} from "firebase/firestore";
+import { collection, doc, getDocs, updateDoc } from "firebase/firestore";
 
 export const getPedidosUsuario = async () => {
   const result = await getDocs(collection(db, "pedidos"));
@@ -19,7 +16,17 @@ export const getPedidosUsuario = async () => {
         cantidad: item.cantidad,
         total: total,
         fecha: item.fecha,
-        estatusEnv: datosEnvio?.estatusEnv
+        estatusEnv: datosEnvio?.estatusEnv,
+        // Agregar datos de la dirección
+        direccion: datosEnvio?.direccion || "",
+        colonia: datosEnvio?.colonia || "",
+        ciudad: datosEnvio?.ciudad || "",
+        cp: datosEnvio?.cp || "",
+        estado: datosEnvio?.estado || "",
+        telefono: datosEnvio?.telefono || "",
+        nombreEnvio: datosEnvio?.nombre || "",
+        // Incluir imagen
+        imagen: item.imagen || "", // Suponiendo que "imagen" está en cada objeto de "carritoCompra"
       }));
 
       return carritoData;
@@ -29,6 +36,34 @@ export const getPedidosUsuario = async () => {
     }
   });
 
+  // Almacenar los ID de los pedidos para uso posterior
+  const pedidosIds = result.docs.map(doc => doc.id);
+  console.log("IDs de los pedidos:", pedidosIds);
+
   console.log("Pedidos obtenidos:", pedidos);
   return pedidos.flat();
 };
+
+
+export const actualizarEstatusPedido = async (pedidoId: string, nuevoEstatus: string) => {
+  if (!pedidoId) {
+    console.error("El ID del pedido es nulo o indefinido.");
+    return;
+  }
+
+  // Construir referencia al documento del pedido utilizando su ID
+  const pedidoRef = doc(db, "pedidos", pedidoId);
+
+  try {
+    // Actualizar el campo estatusEnv dentro del campo datosEnv del documento
+    await updateDoc(pedidoRef, {
+      // Usamos la notación de puntos para acceder a campos anidados
+      "datosEnv.estatusEnv": nuevoEstatus,
+    });
+    console.log("Estatus del pedido actualizado correctamente.");
+  } catch (error) {
+    console.error("Error al actualizar el estatus del pedido:", error);
+    throw error;
+  }
+};
+
--- src/pages/administrador/EstatusEnvioAdmin.tsx
+++ src/pages/administrador/EstatusEnvioAdmin.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Grid,
   Typography,
@@ -9,19 +9,31 @@ import {
   TableHead,
   TableRow,
   Paper,
+  Button,
+  Tooltip,
 } from "@mui/material";
-import { getPedidosUsuario } from "../../config/backEndAdmin/backEstatusAdmin";
+import { getPedidosUsuario, actualizarEstatusPedido } from "../../config/backEndAdmin/backEstatusAdmin";
 
 interface Pedido {
+  id: string; // Identificador único del pedido
   nombre: string;
   cantidad: number;
   total: number;
   fecha: string;
   estatusEnv: string;
+  direccion: string;
+  colonia: string;
+  ciudad: string;
+  cp: string;
+  estado: string;
+  telefono: string;
+  nombreEnvio: string;
+  imagen: string;
 }
 
 export default function EstatusEnvioAdministrador() {
-  const [pedidos, setPedidos] = useState<Pedido[] | null>(null);
+const [pedidos, setPedidos] = useState<Pedido[] | null>(null);
+  const [botonDeshabilitado, setBotonDeshabilitado] = useState(false);
 
   useEffect(() => {
     async function fetchPedidos() {
@@ -36,6 +48,49 @@ export default function EstatusEnvioAdministrador() {
     fetchPedidos();
   }, []);
 
+  const handleActualizarEstatus = async (id: string) => {
+    try {
+      if (!pedidos) return; // Verificar si pedidos existe antes de continuar
+  
+      const pedidoToUpdate = pedidos.find((pedido) => pedido.id === id);
+      if (!pedidoToUpdate) {
+        console.error("Pedido no encontrado.");
+        return;
+      }
+  
+      let nuevoEstatus: string;
+      switch (pedidoToUpdate.estatusEnv) {
+        case "Preparando":
+          nuevoEstatus = "Enviado";
+          break;
+        case "Enviado":
+          nuevoEstatus = "En camino";
+          break;
+        case "En camino":
+          nuevoEstatus = "Entregado";
+          break;
+        default:
+          nuevoEstatus = "Preparando";
+      }
+      
+      await actualizarEstatusPedido(id, nuevoEstatus);
+      const pedidosActualizados = pedidos.map((pedido) => {
+        if (pedido.id === id) {
+          return { ...pedido, estatusEnv: nuevoEstatus };
+        }
+        return pedido;
+      });
+      setPedidos(pedidosActualizados);
+      if (nuevoEstatus === "Entregado") {
+        setBotonDeshabilitado(true);
+      }
+    } catch (error) {
+      console.error("Error al actualizar el estatus del pedido:", error);
+    }
+  };
+  
+  
+
   return (
     <Grid container justifyContent="center">
       <Grid item xs={12} md={10} lg={8}>
@@ -46,32 +101,78 @@ export default function EstatusEnvioAdministrador() {
           <Table aria-label="simple table">
             <TableHead>
               <TableRow>
-                <TableCell>Nombre</TableCell>
-                <TableCell align="right">Cantidad</TableCell>
-                <TableCell align="right">Total</TableCell>
-                <TableCell align="right">Estatus de Envío</TableCell>
+                <TableCell align="left">
+                  <Typography variant="subtitle1" sx={{ fontWeight: "bold" }}>
+                    Nombre
+                  </Typography>
+                </TableCell>
+                <TableCell align="center">
+                  <Typography variant="subtitle1" sx={{ fontWeight: "bold" }}>
+                    Imagen
+                  </Typography>
+                </TableCell>
+                <TableCell align="right">
+                  <Typography variant="subtitle1" sx={{ fontWeight: "bold" }}>
+                    Cantidad
+                  </Typography>
+                </TableCell>
+                <TableCell align="right">
+                  <Typography variant="subtitle1" sx={{ fontWeight: "bold" }}>
+                    Total
+                  </Typography>
+                </TableCell>
+                <TableCell align="center">
+                  <Typography variant="subtitle1" sx={{ fontWeight: "bold" }}>
+                    Detalles
+                  </Typography>
+                </TableCell>
+                <TableCell align="right">
+                  <Typography variant="subtitle1" sx={{ fontWeight: "bold" }}>
+                    Estatus de Envío
+                  </Typography>
+                </TableCell>
+                <TableCell align="right">
+                  <Typography variant="subtitle1" sx={{ fontWeight: "bold" }}>
+                    Actualizar Estatus
+                  </Typography>
+                </TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
               {pedidos &&
-                pedidos.map((pedido: Pedido, index: number) => (
-                  <TableRow key={index}>
+                pedidos.map((pedido: Pedido) => (
+                  <TableRow key={pedido.id}>
                     <TableCell>{pedido.nombre}</TableCell>
+                    <TableCell align="center">
+                      <img
+                        src={pedido.imagen}
+                        alt="Imagen del producto"
+                        style={{ maxWidth: "100px", maxHeight: "100px" }}
+                      />
+                    </TableCell>
                     <TableCell align="right">{pedido.cantidad}</TableCell>
                     <TableCell align="right">{pedido.total}</TableCell>
+                    <TableCell align="center">
+                      <Tooltip
+                        title={`Calle: ${pedido.direccion}, Colonia: ${pedido.colonia}, Ciudad: ${pedido.ciudad}, CP: ${pedido.cp}, Estado: ${pedido.estado}, Teléfono: ${pedido.telefono}, Destinatario: ${pedido.nombreEnvio}`}
+                        arrow
+                      >
+                        <Button variant="contained">Dirección</Button>
+                      </Tooltip>
+                    </TableCell>
                     <TableCell
                       align="right"
                       sx={{
                         color: (() => {
                           switch (pedido.estatusEnv) {
                             case "Preparando":
-                              return "#FFA500"; // Naranja
+                              return "#FFA500";
                             case "En camino":
-                              return "#0000FF"; // Azul
+                              return "#0000FF";
                             case "Enviado":
-                              return "#FFD700"; // Amarillo
+                              return "#FFD700";
                             case "Entregado":
-                              return "#008000"; // Verde
+                              return "#008000";
                             default:
                               return "inherit";
                           }
@@ -80,6 +181,15 @@ export default function EstatusEnvioAdministrador() {
                       }}
                     >
                       {pedido.estatusEnv}
+                    </TableCell>
+                    <TableCell align="right">
+                      <Button
+                        variant="contained"
+                        onClick={() => handleActualizarEstatus(pedido.id)}
+                        disabled={botonDeshabilitado}
+                      >
+                        Actualizar Estatus
+                      </Button>
                     </TableCell>
                   </TableRow>
                 ))}
