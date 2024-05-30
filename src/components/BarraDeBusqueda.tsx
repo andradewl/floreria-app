9cfab72689a45643f335ee8308f2ec2c7a52891f
andradewl <andradeweblab@gmail.com>
2024-05-30 09:35:40 -0600

    cambios busqueda

--- src/components/BarraDeBusqueda.tsx
+++ src/components/BarraDeBusqueda.tsx
@@ -1,22 +1,23 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { Box, Grid, TextField, Typography } from "@mui/material";
 import React from "react";
-import { Flower } from "../interfaces/interfaces";
-import { getProducts } from "../config/apiFirebase";
+import { Flower, Ocasionest } from "../interfaces/interfaces";
+import { getOcasiones, getProducts } from "../config/apiFirebase";
 import { stylesComponents } from "../styles/stylesComponentes";
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 
 
 
 
 function BarraDeBusqueda(){
-    const navigate = useNavigate()
+    // const navigate = useNavigate()
 
 
     const [searchTerm, setSearchTerm] = React.useState('');
     const [searchResults, setSearchResults] = React.useState<Flower[]>([]);
     const [flores, setFlores] = React.useState<Flower[]>([]);
     const [isSearch, setIsSearch] = React.useState(false);
+    const [ocasinesDataId, setOcasinesDataId] = React.useState<Ocasionest[]>([]);
 
 
 
@@ -28,6 +29,8 @@ function BarraDeBusqueda(){
         try {
             const flowersData = await getProducts();
             console.log(flowersData)
+            const ocasionesDataid = await getOcasiones()
+            setOcasinesDataId(ocasionesDataid)
             setFlores(flowersData);
         } catch (error) {
             console.error('Error fetching flowers:', error);
@@ -61,7 +64,9 @@ function BarraDeBusqueda(){
     };
 
     const handleRedirectToProductId = (id:string) => {
-        navigate('/Producto/'+id);
+        const redireccion = "/Producto/"+id
+        window.location.href = redireccion
+        // navigate('/Producto/'+id);
     };
 
     return(
@@ -99,6 +104,137 @@ function BarraDeBusqueda(){
                         <Grid>
                             <Grid container sx={stylesComponents.ContenedorProductos} >
                             {searchResults.map((item) => (
+                                <Grid item xs={6} md={3}  sx={stylesComponents.contenedorProducto}>
+                                    <Box display={'flex'} style={{justifyContent:'center'}}>
+                                        <Grid sx={stylesComponents.contenerdorImagenProducto} onClick={()=>handleRedirectToProductId(item.id)}>
+                                            <img src={item.imagen} alt="" width={'100%'} height={'100%'} style={{ objectFit: 'cover', position:'relative', borderRadius:'7px'}} />
+                                            {item.descuento &&
+                                                <Grid width={'100%'} height={'100%'} sx={{position: 'absolute', textAling:'left' }}>
+                                                    <Box sx={{ backgroundColor:'#ef8f61', width:'50%', color:'white', borderRadius:'5px', fontSize:'20px', margin:'5px' }}>
+                                                        !Oferta¡
+                                                    </Box>
+                                                </Grid>
+                                            }
+                                        </Grid>
+                                    </Box>
+                                    {
+                                        item.descuento ?
+                                        (
+                                            <>
+                                                <Grid
+                                                    container
+                                                    sx={{
+                                                        width: '100%',
+                                                    }}
+                                                >
+                                                    <Grid
+                                                    sx={{
+                                                        width: '100%',
+                                                    }}
+                                                    >
+                                                        <Box sx={{ paddingTop:'2px' }}>
+                                                            <Box sx={{padding:'2px'}} onClick={()=>handleRedirectToProductId(item.id)}>
+                                                                <Typography variant="body1" color="initial" style={{color:'#404040',
+                                                                    fontFamily: "Cormorant",
+                                                                    fontOpticalSizing: "auto",
+                                                                    fontWeight: "bold",
+                                                                    fontStyle: "normal",
+                                                                    textAlign:'left',
+                                                                    fontSize:'17px',
+                                                                    whiteSpace:'nowrap', overflow:'hidden'
+                                                                }}>{item.nombre}</Typography>
+                                                                <Typography variant="body1" color="initial"  style={{color:'#404040',
+                                                                    fontFamily: "Cormorant",
+                                                                    fontOpticalSizing: "auto",
+                                                                    fontWeight: "bold",
+                                                                    fontStyle: "normal",
+                                                                    textAlign:'left',
+                                                                    fontSize:'12px'
+                                                                }}>
+                                                                    {ocasinesDataId && ocasinesDataId.map((item2) => (
+                                                                        item2.id === item.ocasion ? item2.nombre : null
+                                                                    ))}
+                                                                    </Typography>
+                                                            </Box>
+                                                            <Box sx={{display:'flex',padding:'2px',width: '100%',}}>
+                                                                <Typography variant="body2" color="initial"  style={{color:'#404040',textAlign:'left', width:'50%',  textDecorationLine: 'line-through', fontWeight: 'bold', fontSize:'12px' }}>${item.precio}</Typography>
+                                                                <Typography variant="body2" color="initial"  style={{color:'#9c0ba8', textAlign:'right',width:'50%', fontWeight: 'bold',fontSize:'12px' }}>${item.descuento}</Typography>
+                                                            </Box>
+                                                        </Box>
+                                                    </Grid>
+
+                                                </Grid>
+                                            </>
+                                        )
+                                        :
+                                        (
+                                            <Grid
+                                                container
+                                                sx={{
+                                                    width: '100%',
+                                                }}
+                                            >
+                                                <Grid
+                                                    sx={{
+                                                        width: '100%',
+                                                    }}
+                                                >
+                                                    <Box sx={{ padding: {xs:'10px', lg:'2px'} }}>
+                                                        <Box sx={{ padding: '2px' }}>
+                                                            <Typography variant="body1" color="initial" style={{color:'#404040',
+                                                                fontFamily: "Cormorant",
+                                                                fontOpticalSizing: "auto",
+                                                                fontWeight: "bold",
+                                                                fontStyle: "normal",
+                                                                textAlign:'left',
+                                                                fontSize:'17px',
+                                                                whiteSpace:'nowrap', overflow:'hidden'
+                                                            }}>{item.nombre}</Typography>
+                                                            <Typography variant="body1" color="initial"  style={{color:'#404040',
+                                                                fontFamily: "Cormorant",
+                                                                fontOpticalSizing: "auto",
+                                                                fontWeight: "bold",
+                                                                fontStyle: "normal",
+                                                                textAlign:'left',
+                                                                fontSize:'12px'
+                                                            }}>
+                                                                {ocasinesDataId && ocasinesDataId.map((item2) => (
+                                                                    item2.id === item.ocasion ? item2.nombre : null
+                                                                ))}
+                                                            </Typography>
+                                                            {/* <Typography
+                                                                variant="h6"
+                                                                color="initial"
+                                                                fontSize={16}
+                                                                textAlign="center"
+                                                                style={{ color: '#9c0ba8' }}
+                                                            >
+                                                            ${item.precio}
+                                                            </Typography> */}
+                                                        </Box>
+                                                        <Box
+                                                        >
+                                                            {/* <Typography
+                                                                variant="h6"
+                                                                color="initial"
+                                                                fontSize={16}
+                                                                style={{ color: '#404040' }}
+                                                            >
+                                                            {item.nombre} */}
+                                                            {/* </Typography> */}
+                                                            <Typography variant="body2" color="initial"  style={{color:'#9c0ba8', textAlign:'left', fontWeight: 'bold',fontSize:'12px' }}>${item.precio }</Typography>
+                                                        </Box>
+                                                    </Box>
+                                                </Grid>
+                                            </Grid>
+
+                                        )
+
+                                    }
+
+                                </Grid>
+                            ))}
+                            {/* {searchResults.map((item) => (
                                 <Grid item xs={6} md={3}  sx={stylesComponents.contenedorProducto}>
                                     <Box display={'flex'} style={{justifyContent:'center'}}>
                                         <Grid sx={stylesComponents.contenerdorImagenProducto} onClick={()=>handleRedirectToProductId(item.id)}>
@@ -172,10 +308,6 @@ function BarraDeBusqueda(){
                                                     }}
                                                 >
                                                     <Box sx={{ padding: {xs:'10px', lg:'2px'} }}>
-                                                        {/* <Box sx={{ justifyContent: 'center', textAlign: 'center' }}>
-                                                            <FavoriteBorderIcon sx={{ width: '50%' }} />
-                                                            <RemoveRedEyeIcon sx={{ width: '50%' }} />
-                                                        </Box> */}
                                                         <Box sx={{ padding: '2px' }}>
                                                             <Typography
                                                                 variant="h6"
@@ -207,7 +339,7 @@ function BarraDeBusqueda(){
                                     }
 
                                 </Grid>
-                            ))}
+                            ))} */}
 
                         </Grid>
                     </Grid>
